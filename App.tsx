34853d58def799eec69a9acc02e25ff4ebc88faa
Danricode <danielaudrasilva@gmail.com>
2022-06-30 18:49:18 +0200

    Initial setup

---
+++ App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { View,Text } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import 'react-native-gesture-handler';
+
+
+export default function App() {
+  return (
+    
+    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+  );
+}
